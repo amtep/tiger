c04d78d5514422a24872893ea1048814fc20a552
Richard Braakman <shuttlegit@gmail.com>
2026-02-12 20:56:14 +0200

    vic3: update to 1.12.4

--- vic3-tiger/src/lib.rs
+++ vic3-tiger/src/lib.rs
@@ -1,10 +1,9 @@
 use tiger_bin_shared::GameConsts;
 
-// LAST UPDATED VIC3 VERSION 1.12.3
 pub const GAME_CONSTS: &GameConsts = &GameConsts {
     name: "Victoria 3",
     name_short: "Vic3",
-    version: "1.12.3 (Chá de Tília)",
+    version: "1.12.4 (Chá de Tília)",
     app_id: 529_340,
     signature_file: "game/events/titanic_events.txt",
     paradox_dir: "Victoria 3",
