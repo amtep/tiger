74912b1cc3ac8155c50c6c259612f883d3c448ae
Richard Braakman <shuttlegit@gmail.com>
2026-02-19 19:07:45 +0200

    eu5: fill in ModifKinds field

--- src/eu5/modif.rs
+++ src/eu5/modif.rs
@@ -10,9 +10,17 @@ bitflags! {
     #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
     #[rustfmt::skip]
     pub struct ModifKinds: u32 {
-        const Character         = 1<<0;
-        const Country           = 1<<1;
-        // TODO: EU5 fill in table
+        const None = 1<<0;
+        const Character = 1<<1;
+        const Country = 1<<2;
+        const Dynasty = 1<<3;
+        const InternationalOrganization = 1<<4;
+        const Location = 1<<5;
+        const Mercenary = 1<<6;
+        const Province = 1<<7;
+        const Rebel = 1<<8;
+        const Religion = 1<<9;
+        const Unit = 1<<10;
     }
 }
 
@@ -29,13 +37,39 @@ impl crate::modif::ModifKinds for ModifKinds {
 impl Display for ModifKinds {
     fn fmt(&self, f: &mut Formatter) -> Result<(), std::fmt::Error> {
         let mut vec = Vec::new();
+        if self.contains(ModifKinds::None) {
+            vec.push("None");
+        }
         if self.contains(ModifKinds::Character) {
-            vec.push("character");
+            vec.push("Character");
         }
         if self.contains(ModifKinds::Country) {
-            vec.push("country");
+            vec.push("Country");
         }
-        // TODO: EU5 add new modifkinds here
+        if self.contains(ModifKinds::Dynasty) {
+            vec.push("Dynasty");
+        }
+        if self.contains(ModifKinds::InternationalOrganization) {
+            vec.push("InternationalOrganization");
+        }
+        if self.contains(ModifKinds::Location) {
+            vec.push("Location");
+        }
+        if self.contains(ModifKinds::Mercenary) {
+            vec.push("Mercenary");
+        }
+        if self.contains(ModifKinds::Province) {
+            vec.push("Province");
+        }
+        if self.contains(ModifKinds::Rebel) {
+            vec.push("Rebel");
+        }
+        if self.contains(ModifKinds::Religion) {
+            vec.push("Religion");
+        }
+        if self.contains(ModifKinds::Unit) {
+            vec.push("Unit");
+        }
         write!(f, "{}", vec.join(", "))
     }
 }
