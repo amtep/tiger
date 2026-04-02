e7188339d5fa0f64b9b58546724086137ca338a2
Richard Braakman <shuttlegit@gmail.com>
2026-04-02 18:27:12 +0300

    lsp: send trace logs to stderr instead of file
    This is safer than a file in /tmp, and the client (helix)
    will log them for us.

--- tiger-lsp/src/main.rs
+++ tiger-lsp/src/main.rs
@@ -1,15 +1,17 @@
-use std::fs::File;
-
 use anyhow::Result;
 use log::error;
-use simplelog::{CombinedLogger, Config, LevelFilter, WriteLogger};
+use simplelog::{ColorChoice, CombinedLogger, Config, LevelFilter, TermLogger, TerminalMode};
 
 use tiger_lsp::{ConnectionLogger, main_loop::main_loop};
 
 fn main() -> Result<()> {
     CombinedLogger::init(vec![
-        // TODO: find a proper home for the logfile
-        WriteLogger::new(LevelFilter::Trace, Config::default(), File::create("/tmp/lsp.log")?),
+        TermLogger::new(
+            LevelFilter::Trace,
+            Config::default(),
+            TerminalMode::Stderr,
+            ColorChoice::Never,
+        ),
         ConnectionLogger::new(LevelFilter::Warn),
     ])?;
 
